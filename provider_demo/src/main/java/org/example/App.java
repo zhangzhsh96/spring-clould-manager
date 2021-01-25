578559b88b26e12e94c1055b2b4c9201cbc0b803
zhangzhsh <897042405@qq.com>
2021-01-25 22:28:44 +0800

    add sout

--- provider_demo/src/main/java/org/example/App.java
+++ provider_demo/src/main/java/org/example/App.java
@@ -7,10 +7,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class App 
-{
-    public static void main( String[] args )
-    {
-        SpringApplication.run(App.class,args);
+public class App {
+    public static void main(String[] args) {
+        System.out.println("===g");
+        SpringApplication.run(App.class, args);
     }
 }
