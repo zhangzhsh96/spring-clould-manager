2558fe08dbb4218d10870704c82dca5d07759637
zhangzhsh <897042405@qq.com>
2021-01-26 08:17:32 +0800

    add Test1

--- provider_demo/src/main/java/org/example/controller/Test1.java
+++ provider_demo/src/main/java/org/example/controller/Test1.java
@@ -2,6 +2,7 @@ package org.example.controller;
 
 public class Test1 {
     private String adds;
+    private int num;
     public static void main(String[] args) {
         System.out.println();
         System.out.println("2222");
