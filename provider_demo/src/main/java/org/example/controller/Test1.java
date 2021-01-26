f7f301d6cb6ce589f0b5e39ae84d99f8a1b6a43b
zhangzhsh <897042405@qq.com>
2021-01-26 08:24:33 +0800

    rm num

--- provider_demo/src/main/java/org/example/controller/Test1.java
+++ provider_demo/src/main/java/org/example/controller/Test1.java
@@ -2,7 +2,6 @@ package org.example.controller;
 
 public class Test1 {
     private String adds;
-    private int num;
     public static void main(String[] args) {
         System.out.println();
         System.out.println("2222");
